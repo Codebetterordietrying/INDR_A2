8af6a0d25890453bb7079b6e6f3ca625e536c76c
yvesg <yvesg@192.168.20.28>
2023-10-15 19:43:34 +1100

    stuff

---
+++ main.m
@@ -0,0 +1,8 @@
+% LAB ASSESSMENT 2: CHEFMATE -MAIN PROGRAM
+% Main program that runs and coordinates all sub-programs for the task
+% Author: Yves Gayagay, Michele Liang, Rohit Bhat
+% Rev: 1.0
+
+
+%% Load the Environment 
+env=env();
