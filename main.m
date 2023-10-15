39a0565508a87d465db8090a4b494a1dd32ec006
yvesg <yvesg@DESKTOP-EBDU47V.modem>
2023-10-16 01:13:54 +1100

    rev1

--- main.m
+++ main.m
@@ -8,6 +8,12 @@ run("C:\Windows\System32\rvctools\startup_rvc.m");
 
 %% Load the Linear UR5 model, Our Robot model, Environment and Models
 
+   h = PlaceObject('table_test.ply',[0,0,0]);
+            verts = [get(h,'Vertices'), ones(size(get(h,'Vertices'),1),1)] * troty(pi/2)*trotx(-pi/2);
+            set(h,'Vertices',verts(:,1:3))
+axis equal
+
 %Origin pose
 og=eye(4);
 UR5=LinearUR5(og);
+UR5.model.teach(zeros(1,UR5.model.n))
