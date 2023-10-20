0f38377ff1ae44d3529ea023f79b3ec7eb4b9b3a
yvesg <yvesg@DESKTOP-EBDU47V.modem>
2023-10-20 21:08:24 +1100

    working on picking things up (atm, theres a offset)

--- Env.m
+++ Env.m
@@ -4,22 +4,102 @@
 % Rev: 1.0
 
 %% Env Class
-classdef Env
-  
-    
-    properties 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PROPERTIES
+
+% path          :   Pathfile of the model in the directory
+% model         :   Name of the Model used
+% init          :   Initial Coordinates of Cartersian 
+% static        :   Static status , 0: Does not move 1: Will move
+
+% METHODS
+
+% plot()        :   Implement models onto 3D-graph. Must be called FIRST
+%                   Input:  4x4 Homogeneous Matrix i.e: eye(4), transl()...
+% 
+% update()      :   Update the pose of the models in the 3D-graph.
+%                   Static bool must be 1. If 0, an error occurs
+%                   Input:  4x4 Homogeneous Matrix i.e.: eye(4), transl()..
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+classdef Env < handle
+  properties
+        
+         path       
+
+         model       
+
+        init=[]    
+            
+        static     
+        
+        
     end
     
 
 
-    methods
-        function self = Env()
-            
-            
-        end
-        
+    methods 
+        function self = Env(path,model,init,static) % Constructor to initialize models and set in class
+              %input: File path in string format, custom name, initial coordinates, bool for static
 
+            % Copy the attributes of the model and set them to the class of its own
+
+            self.path=path;
+
+            self.model=model;
+
+            if nargin==2
+
+            self.init=[0 0 0];
+            self.static=0;
+            else
+           
+            
+            self.init=init;
+            self.static=static;  % 0= Not moving    1= Will move
+            end        
+        end
+
+
+
+        function [handle,vertices] = plot(self,tr) %Function to plot the initialized models onto the graph 
+           % IN YZX FORMAT   [   X=Y, Y=Z , Z=X ]
+            % Input: 4x4 Homogenous Matrix
+
+          
+                    
+           handle = PlaceObject(self.path);
+           vertices = get(handle,'Vertices');
+          
+          if nargin==2 
+
+
+           transformedVertices = [vertices,ones(size(vertices,1),1)] * tr';
+           set(handle,'Vertices',transformedVertices(:,1:3));
+          
+          elseif nargin==1
+           transformedVertices = [vertices,ones(size(vertices,1),1)] * eye(4);
+           set(handle,'Vertices',transformedVertices(:,1:3));   
+          end
+
+        end
+
+    
+
+
+
+        function update(self,handles,vertices,tr) %Use this function to update transform of the model (When animating)
+            % Input: modle_handle , modle_vertices matrix, 4x4 Homogenous Matrix
+        
+        if self.static==1
+        transformedVertices1 = [vertices,ones(size(vertices,1),1)] * tr';
+        set(handles,'Vertices',transformedVertices1(:,1:3));
+                
+        else
+        
+        error("Model is static, change static status if the model will move");
+        end
         
     end
 end
-
+end
--- main.m
+++ main.m
@@ -6,56 +6,49 @@
 %% Load the toolbox (If installed in different directory, load it outside of this script)
 run("C:\Windows\System32\rvctools\startup_rvc.m");
 
-%% Load the Linear UR5 model, Our Robot model, Environment and Models
 
-workspace=[-2 2 -2 3 -0.5 3];
-name='linearur5';
+%% Load the Linear UR5 model, Our Robot model, Environment and Models
+%Initialize Our Main Robot- Linear UR5
 
 UR5=OurLinearUR5;
 q1=[-0.01 0 0 0 0 0 0];
-UR5.model.plot3d(q1,'nowrist','noarrow','notiles','workspace',workspace,'scale',0.25,'view','x','fps',60,'alpha',0);
+workspace=[-2 2 -2 3 -0.5 3];
+UR5.model.plot3d(q1,'notiles','nowrist','noarrow','workspace',workspace,'scale',0.25,'view','x','fps',60,'alpha',0);
 
 hold on
+% Initialize Main Environmment - Kitchen and Restaurant
+
+  table= Env("Environment\Env\table_test_v2.ply","table",[0 0 0],0);
+  table.plot(trotx(pi/2)*troty(3*pi/2)*transl(-0.25,-0.05,0));
    
-h = PlaceObject("Environment\Env\table_test_v2.ply",[-0.25,-0.015,0]);
-            verts = [get(h,'Vertices'), ones(size(get(h,'Vertices'),1),1)] * troty(pi/2)*trotx(-pi/2);
-            set(h,'Vertices',verts(:,1:3))
-   
+% Initialize Models for Restaurant 
 
-   %Spawn a Large burger on its station
+  lrgburg= Env("Environment\Mdl\Restaurant\hamburgerLRG.ply","large_burger",[-0.2,1.4,-0.1],1);
+  [lb_h,lb_v] =lrgburg.plot(transl(-0.2,1.4,-0.1));
 
-   h1 = PlaceObject("Environment\Mdl\Restaurant\hamburgerLRG.ply",[-0.3,0.025,-0.05]);
-            verts = [get(h1,'Vertices'), ones(size(get(h1,'Vertices'),1),1)]*trotz(pi/2);
-            set(h1,'Vertices',verts(:,1:3))
+  burg= Env("Environment\Mdl\Restaurant\hamburger.ply","Burger",[0 0 0],1);
+  burg.plot(transl(0.05,1.4,-0.1));
 
+  fries= Env("Environment\Mdl\Restaurant\fries.ply","fries",[0 0 0],1);
+  fries.plot(transl(0.3,0.95,-0.1));
 
-    %spawn a small burger on its station
+  axis equal
 
-     h2 = PlaceObject("Environment\Mdl\Restaurant\hamburger.ply",[-0.3,0.24,-0.05]);
-            verts = [get(h2,'Vertices'), ones(size(get(h2,'Vertices'),1),1)]*trotz(pi/2);
-            set(h2,'Vertices',verts(:,1:3))
-        
-     h3 = PlaceObject("Environment\Mdl\Restaurant\fries.ply",[-0.15,0.01,-0.05]);
-            verts = [get(h3,'Vertices'), ones(size(get(h3,'Vertices'),1),1)]*trotz(pi/2);
-            set(h3,'Vertices',verts(:,1:3))
-
-     
-
-
-%Origin pose and joint angles
-
-
-axis equal
-
-%% Move the Robot through teach() and getpos() REV 1.0      (Subjected to change)
-
+%%
 
 qb1=[-0.3329    3.0917    0.9225    0.3532    0.4488    1.6955         0]; % Pose of the waypoint
 qb2=[-0.6683    3.0543    0.6912   -1.0978   -0.2409    1.5708         0]; % Pose of hovering Reg Burger
+qb3=[-0.0100   -3.6880    1.1610   -0.1290    0.2732    1.6391         0]; % Pose to drop off on tray 1
 
-% stuff like thnis
+
+
+% q matrices of paths
+
 qmatrix1=jtraj(q1,qb1,50);
 qmatrix2=jtraj(qb1,qb2,50);
+qmatrix3=jtraj(qb2,qb1,50);
+dropmatrix=jtraj(qb1,qb3,50);
+
 
 while 1
 for i=1:50
@@ -73,7 +66,40 @@ for i=1:50
    axis equal
 
 end
+
+
+for i=1:50
+    
+  UR5.model.animate(qmatrix3(i,:));
+  tr=UR5.model.fkine(qmatrix3(i,:));
+
+  ttr=[tr.n tr.o tr.a tr.t ; 0 0 0 1];
+
+ lrgburg.update(lb_h,lb_v,ttr);
+
+   pause(0.01);
+   axis equal
+
 end
+
+for i=1:50
+    
+   UR5.model.animate(dropmatrix(i,:));
+   drawnow();
+   axis equal
+
+end
+
+
+
+end
+
+
+
+
+%for i = 1:50
+  
+%end
 
 % Notepad: 
 
@@ -86,5 +112,60 @@ end
 
 
 % WAYPOINTS
-% Fstat Wayp  = [-0.3329    3.0917    0.9225    0.3532    0.4488    1.6955
-% 0]
+% Fstat Wayp  = [-0.3329    3.0917    0.9225    0.3532    0.4488    1.6955         0]
+
+% Dropoff
+% Tray 1      = [-0.0100   -3.6880    1.1610   -0.1290    0.2732    1.6391         0]
+
+
+
+
+%% test
+
+UR51=OurLinearUR5;
+q1=[-0.01 0 0 0 0 0 0];
+workspace=[-2 2 -2 3 -0.5 3];
+UR51.model.plot3d(q1,'nowrist','noarrow','notiles','workspace',workspace,'scale',0.25,'view','x','fps',60,'alpha',0);
+
+hold on
+
+
+h1 = PlaceObject("Environment\Mdl\Restaurant\hamburgerLRG.ply",[-0.2,1.4,-0.1]);
+vertices = get(h1,'Vertices');
+
+            transformedVertices = [vertices,ones(size(vertices,1),1)] * transl(0.3,0.025,-0.05);
+            set(h1,'Vertices',transformedVertices(:,1:3));
+
+                     
+
+qb1=[-0.3329    3.0917    0.9225    0.3532    0.4488    1.6955         0]; % Pose of the waypoint
+qb2=[-0.6683    3.0543    0.6912   -1.0978   -0.2409    1.5708         0]; % Pose of hovering Reg Burger
+qb3=[-0.0100   -3.6880    1.1610   -0.1290    0.2732    1.6391         0]; 
+
+qmatrix1=       jtraj(q1,qb1,50);
+qmatrix2=       jtraj(qb1,qb2,50);
+qmatrix3=       jtraj(qb2,qb1,50);
+dropmatrix=     jtraj(qb1,qb3,50);
+
+
+axis equal
+while 1
+
+for i=1:50
+    
+    UR51.model.animate(qmatrix3(i,:));
+    transform= UR51.model.fkine(qmatrix3(i,:));
+
+    hom=[transform.n transform.o transform.a transform.t ; 0 0 0 1];
+
+    transformedVertices = [vertices,ones(size(vertices,1),1)] * hom';
+    set(h1,'Vertices',transformedVertices(:,1:3));
+
+   drawnow();
+
+
+
+end
+
+end
+
